dd85c94d2fde633875bdc5328cded4baad09b3f5
Rezuan <hasanideal2002@gmail.com>
2026-04-03 23:27:55 +0600

    Initial Commit

---
+++ NewsBit/NewsBitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsBitApp.swift
+//  NewsBit
+//
+//  Created by Rezuan Mustafa Hasan on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct NewsBitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
